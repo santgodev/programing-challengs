0e96000808b6510c2bebde342a88ea89d2ac2e92
santgodev <165436594+santgodev@users.noreply.github.com>
2025-08-20 22:50:11 -0500

    Update el-tren-magico.ts

--- el-tren-magico.ts
+++ el-tren-magico.ts
@@ -1,63 +1,143 @@
-type Board = string[]
-type Movement = 'U' | 'D' | 'R' | 'L'
-type Result = 'none' | 'crash' | 'eat'
+type Board = string[];
+type Movement = 'U' | 'D' | 'R' | 'L';
+type Result = 'none' | 'crash' | 'eat';
 
-const board = [
-  '·····',
-  '*····',
-  '@····',
-  'o····',
-  'o····'
-]
+let boardMain = [
+  '······',
+  '······',
+  '··@···',
+  '·*oo··',
+  '······',
+  '······'
+];
 
-function moveTrain(board: Board, mov: Movement): Result {
-let dot = '.';
+function moveTrain(board: Board, mov: Movement) : Result{
+  let boardMatriz: string[][] = [];
+  let result : Result = "none";
+  let positionAt: [index: number, indexInRow: number] = [0, 0];
+  let positionDot1: [index: number, indexInRow: number] = [0, 0];
+  let positionDot2: [index: number, indexInRow: number] = [0, 0];
+  let positionOfFruit: [index: number, indexInRow: number] = [0, 0];
 
-
-let positionAt: [index:number, indexInRow:number] = [0, 0];
-let positionDot1: [index:number, indexInRow:number] = [0, 0];
-let positionDot2: [index:number, indexInRow:number] = [0, 0];
-let positionOfFruit: [index:number, indexInRow:number] = [0, 0];
- 
-for(let i:number=0; i<board.length;i++){
-  for(let d:number=0; d<board[i].length;d++){
-    if(board[i][d]=='@'){positionAt=[i,d];break}
+  // Crear matriz y detectar posiciones
+  for (let x: number = 0; x < board.length; x++) {
+    boardMatriz.push([]);
+    for (let y: number = 0; y < board[0].length; y++) {
+      if (board[x][y] == '@') { positionAt = [x, y]; }
+      if (board[x][y] == '*') { positionOfFruit = [x, y]; }
+      boardMatriz[x][y] = board[x][y];
+    }
   }
+
+  // Buscar Dot1
+  if (positionAt[1] > 0) {
+    if (boardMatriz[positionAt[0]][positionAt[1] - 1] == "o") {
+    positionDot1 = [positionAt[0], positionAt[1] - 1];
+    }
+  }
+  if (positionAt[1] < boardMatriz[0].length) {
+    if (boardMatriz[positionAt[0]][positionAt[1] + 1] == "o") {
+    positionDot1 = [positionAt[0], positionAt[1] + 1];
+    }
+  }         
+
+  if (positionAt[0] > 0) {
+    if (boardMatriz[positionAt[0] - 1][positionAt[1]] == "o") {
+      positionDot1 = [positionAt[0] - 1, positionAt[1]];
+    }
+  }
+  if (positionAt[0] < boardMatriz.length -1) {
+    if (boardMatriz[positionAt[0] + 1][positionAt[1]] == "o") {
+      positionDot1 = [positionAt[0] + 1, positionAt[1]];
+    }
+  }
+
+  // Buscar Dot2
+  if (positionDot1[1] > 0) {
+    if (boardMatriz[positionDot1[0]][positionDot1[1] - 1] == "o") {
+    positionDot2 = [positionDot1[0], positionDot1[1] - 1];
+    }
+  }
+
+  if (positionDot1[1] < boardMatriz[0].length) {
+    if (boardMatriz[positionDot1[0]][positionDot1[1] + 1] == "o") {
+    positionDot2 = [positionDot1[0], positionDot1[1] + 1];
+    }
+  }
+  if (positionDot1[0] > 0) {
+    if (boardMatriz[positionDot1[0] - 1][positionDot1[1]] == "o") {
+      positionDot2 = [positionDot1[0] - 1, positionDot1[1]];
+    }
+  }
+
+  if (positionDot1[0] < boardMatriz.length - 1) {
+  if (boardMatriz[positionDot1[0] + 1][positionDot1[1]] == "o") {
+    positionDot2 = [positionDot1[0] + 1, positionDot1[1]];
+    }
+  }
+
+
+
+  console.log("position @ " + positionAt);
+  console.log("position Dot 1 " + positionDot1);
+  console.log("position Dot 2 " + positionDot2);
+
+  switch (mov) {
+    case "U":
+      if (positionAt[0] == 0) {{result = "crash"} break;}
+      if (boardMatriz[positionAt[0] - 1][positionAt[1]] == "o") {result = "crash"; break;}
+
+      boardMatriz[positionAt[0] - 1][positionAt[1]] = "@";
+      boardMatriz[positionAt[0]][positionAt[1]] = "o";
+      boardMatriz[positionDot2[0]][positionDot2[1]] = "·";
+      break;
+
+    case "D":
+      if (positionAt[0]==boardMatriz.length-1) {result = "crash"; break;}
+      if (boardMatriz[positionAt[0] + 1][positionAt[1]] == "o") {result = "crash"; break;}
+
+      boardMatriz[positionAt[0] + 1][positionAt[1]] = "@";
+      boardMatriz[positionAt[0]][positionAt[1]] = "o";
+      boardMatriz[positionDot2[0]][positionDot2[1]] = "·";
+      break;
+
+    case "L":
+      if (positionAt[1] == 0) {result = "crash"; break;}
+      if (boardMatriz[positionAt[0]][positionAt[1] - 1] == "o") {result = "crash"; break;}
+
+      boardMatriz[positionAt[0]][positionAt[1] - 1] = "@";
+      boardMatriz[positionAt[0]][positionAt[1]] = "o";
+      boardMatriz[positionDot2[0]][positionDot2[1]] = "·";
+      break;
+
+    case "R":
+      if (positionAt[1] == boardMatriz[0].length - 1){ result = "crash"; break;}
+      if (boardMatriz[positionAt[0]][positionAt[1] + 1] == "o") {result = "crash"; break;}
+
+      boardMatriz[positionAt[0]][positionAt[1] + 1] = "@";
+      boardMatriz[positionAt[0]][positionAt[1]] = "o";
+      boardMatriz[positionDot2[0]][positionDot2[1]] = "·";
+      break;
+
+  }
+  for (let x: number = 0; x < board.length; x++) {
+    for (let y: number = 0; y < board[0].length; y++) {
+      if (boardMatriz[x][y] == '@') { positionAt = [x, y]; }
+    }
+  }
+  if(positionAt[0]==positionOfFruit[0] && positionAt[1]==positionOfFruit[1]) result = "eat";
+  
+  
+
+  console.log("-------------------------");
+
+  const str: string[] = [];
+  boardMatriz.forEach((row) => {
+    let rowString: string = row.join('');
+    str.push(rowString);
+    console.log(rowString);
+  });
+
+  boardMain = str;
+  return result;
 }
-for(let i:number=0; i<board.length;i++){
-  for(let d:number=0; d<board[i].length;d++){
-    if(board[i][d]=='o'){positionDot1=[i,d];break}
-  }
-}
- for(let i:number=board.length-1; i>=0;i--){
-  for(let d:number=board[i].length-1; d>=0;d--){
-    if(board[i][d]=='o'){positionDot2=[i,d];break}
-  }
-}  
-for(let i:number=board.length-1; i<0;i++){
-  for(let d:number=board[i].length-1; d>=0;d++){
-    if(board[i][d]=='*'){positionOfFruit=[i,d];break}
-  }
-}
-   switch(mov){
-    case 'U':
-    board[positionAt[0]-1] = dot.repeat(positionAt[1]) + '@' + dot.repeat(board[0].length-positionAt[1])
-    board[positionAt[0]] = dot.repeat(positionAt[1]) + 'o' + dot.repeat(board[0].length-positionAt[1])
-    board[positionDot1[0]] = dot.repeat(positionDot1[1]) + 'o' + dot.repeat(board[0].length-positionDot1[1])
-    break;
-    case 'D':
-    break;
-    case 'R':
-    break;
-    case 'L':
-    break;
-  }
- board.forEach((board)=>{
-console.log(board)
- })
-
-  return 'none';
-}
-
-
-moveTrain(board, 'U')
