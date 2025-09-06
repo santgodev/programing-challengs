82f5c4264a25751ec0423f9940bfa3e37ded2670
santgodev <165436594+santgodev@users.noreply.github.com>
2025-09-06 00:15:25 -0500

    Update el_robot_esta_de_vuelta.ts

--- el_robot_esta_de_vuelta.ts
+++ el_robot_esta_de_vuelta.ts
@@ -19,89 +19,107 @@ function isRobotBack(moves: string): true | [number, number] {
   type Coord = [number, number];
 
   let board: Coord[] = [[0, 0]]; 
-  let realMove: string = "";
+  let history: string[] = []; 
 
   for (let i: number = 0; i < moves.length;) {
-    let [x, y] = board[i];
+    let [x, y] = board[board.length-1];
 
     if (moves[i] == "L") {
-      board.push([x, y - 1]);
+      history.push("L");
+      board.push([x - 1, y ]);
     }
     if (moves[i] == "R") {
-      board.push([x, y + 1]);
-    }
-    if (moves[i] == "U") {
-      board.push([x - 1, y]);
-    }
-    if (moves[i] == "D") {
+      history.push("R");
       board.push([x + 1, y]);
     }
+    if (moves[i] == "U") {
+      history.push("U");
+      board.push([x , y + 1]);
+    }
+    if (moves[i] == "D") {
+      history.push("D");
+      board.push([x , y - 1]);
+    }
     if (moves[i] == "*") {
+
       if (moves[i+1] == "L") {
-        board.push([x, y + 2]);
+        history.push("L");
+        board.push([x - 2, y ]);
       }
       if (moves[i+1] == "R") {
-        board.push([x, y - 2]);
-      }
-      if (moves[i+1] == "U") {
+        history.push("R");
         board.push([x + 2, y]);
       }
-      if (moves[i+1] == "D") {
-        board.push([x - 2, y]);
+      if (moves[i+1] == "U") {
+        history.push("U");
+        board.push([x, y + 2]);
       }
-      i+1;
+      if (moves[i+1] == "D") {
+        history.push("D");
+        board.push([x, y - 2]);
+      }
+      i++;
+      
     }
     if (moves[i] == "!") {
       if (moves[i+1] == "L") {
-        board.push([x, y + 1]);
-      }
-      if (moves[i+1] == "R") {
-        board.push([x, y - 1]);
-      }
-      if (moves[i+1] == "U") {
+        history.push("R");
         board.push([x + 1, y]);
       }
-      if (moves[i+1] == "D") {
-        board.push([x - 1, y]);
+      if (moves[i+1] == "R") {
+        history.push("L");
+        board.push([x -1, y]);
       }
-      i+1;
+      if (moves[i+1] == "U") {
+        history.push("D");
+        board.push([x, y - 1]);
+      }
+      if (moves[i+1] == "D") {
+        history.push("U");
+        board.push([x, y + 1]);
+      }
+      i++;
     }
     if (moves[i] == "?") {
-        console.log("este es x y y ",x, y)
       let isPresent : boolean = false;
-      for(let g:number = 0; g<i;g++){
-       if( moves[g]==moves[i+1]){
-        console.log("finalmente se cumplio");
-        board.push([x, y]);
+      for(let g:number = 0; g<history.length;g++){
+       if( history[g]==moves[i+1]){
         isPresent = true;
         i++;
         break;
        }
-
-      if(isPresent){
-        console.log("hola mundo cruel")
-      if (moves[i+1] == "L") {
-        board.push([x, y - 1]);
       }
-      if (moves[i+1] == "R") {
-        board.push([x, y + 1]);
-      }
-      if (moves[i+1] == "U") {
+      if(!isPresent){
+        if (moves[i+1] == "L") {
+        history.push("L");
         board.push([x - 1, y]);
-      }
-      if (moves[i+1] == "D") {
+        }
+        if (moves[i+1] == "R") {
+        history.push("R");
         board.push([x + 1, y]);
+        }
+        if (moves[i+1] == "U") {
+        history.push("U");
+        board.push([x, y + 1]);
+        }
+        if (moves[i+1] == "D") {
+        history.push("D");
+        board.push([x, y - 1]);
       }
       i++;
        }
-      }
 
     }
       i++;
-        console.log(board[i-1]);
   }
+
   
-  return true;
+  if(board[board.length-1][0] == board[0][0] && board[board.length-1][1] == board[0][1] ){
+    return true
+  }
+  return board[board.length-1]
+
 }
+
 
 isRobotBack(str);
